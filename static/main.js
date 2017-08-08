0b5daf356135dd33a70410d1dfa2f3ead96dee50
Stanislav Lesiuk <stanislav.lesiuk@gmail.com>
2017-08-08 09:40:43 +0200

    minor styles improvement

--- static/main.js
+++ static/main.js
@@ -12,16 +12,14 @@ displayProducts(products)
 function displayProducts (products) {
   productsRoot.empty()
   products.forEach(product => {
-    const node = $('<div />')
-    const title = $('<div />')
-    const desc = $('<div />')
-    const more = $('<div />')
+    const node = $('<div class="product product--small"/>')
+    const title = $('<h2 class="product__title"/>')
+    const desc = $('<div class="product__desc"/>')
+    const more = $('<button type="button" class="product__read-more"/>')
     const img = $('<img />')
-    const price = $('<div />')
+    const price = $('<div class="product__price"/>')
 
-    node.addClass('product product--small')
     title.text(product.title)
-    desc.addClass('product__desc')
     desc.text(product.desc)
     more.text('Read more')
     more.click(function () {
@@ -30,7 +28,6 @@ function displayProducts (products) {
     })
     img.attr('src', product.img)
     price.text(product.price + ' EUR')
-    price.addClass('product__price')
 
     node.append(img)
     node.append(title)
